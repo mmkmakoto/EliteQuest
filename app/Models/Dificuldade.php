22114117f3f7c33c5d2dfac9f5939ab956c7772d
Marcos Makoto Kato <mmkmakoto@gmail.com>
2017-05-27 16:52:24 -0300

    iniciando os models

---
+++ app/Models/Dificuldade.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+class Questao extends Model
+{
+	protected $table = "questoes";
+	protected $fillable = [
+	    'categoria_id',
+	    'titulo',
+	    'opcao_2',
+	    'opcao_3',
+	    'opcao_4',
+	    'status',
+	];
+}
