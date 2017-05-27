12e4f506544c3e208c6d871920f7626a96785aa7
Felipe Mariano <felipemarianodasilva@gmail.com>
2017-05-27 16:51:21 -0700

    criação de modelo de parametros

---
+++ public/views/partida/parameters.js
@@ -0,0 +1,66 @@
+var status = {
+  "player_1": {
+    "jogador": {
+      "nome": "Daniel",
+      "posicao": 3,
+      "pino": "verde",
+    },
+    "fichas" : {
+      "1": {
+        "valor": 1,
+        "disponivel": true
+      },
+      "2": {
+        "valor": 2,
+        "disponivel": false
+      },
+      "3": {
+        "valor": 3,
+        "disponivel": true
+      },
+      "4": {
+        "valor": 4,
+        "disponivel": false
+      },
+      "5": {
+        "valor": 5,
+        "disponivel": true
+      },
+      "6": {
+        "valor": 6,
+        "disponivel": true
+      }
+    },
+    "player_2": {
+      "jogador": {
+        "nome": "Makoto",
+        "posicao": 13,
+        "pino": "amarelo",
+      },
+      "fichas" : {
+        "1": {
+          "valor": 1,
+          "disponivel": true
+        },
+        "2": {
+          "valor": 2,
+          "disponivel": true
+        },
+        "3": {
+          "valor": 3,
+          "disponivel": true
+        },
+        "4": {
+          "valor": 4,
+          "disponivel": true
+        },
+        "5": {
+          "valor": 5,
+          "disponivel": true
+        },
+        "6": {
+          "valor": 6,
+          "disponivel": true
+        }
+      }
+}
