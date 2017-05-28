b0b837497aa5e68eecf5e0996e5e712fbe08fc13
Felipe Mariano <felipemarianodasilva@gmail.com>
2017-05-27 21:38:52 -0700

    padronizacao inicial jsons

--- public/views/partida/parameters.js
+++ public/views/partida/parameters.js
@@ -1,62 +1,75 @@
 
-
-/// Data model:
-
 var $status = {
-  "player_1": {
-    "jogador": {
-      "seu_turno": true,
-      "nome": "Daniel",
-      "posicao": 3,
-      "pino": "verde",
-      "questoes_respondidas": 3
-    },
-    "fichas" : {
-      "1": {
-        "valor": 1,
-        "disponivel": true
+  "rodada": {
+    "id": 3,
+    "pergunta": {
+      "id": 1,
+      "titulo": "esse é o titulo",
+      "opcao_1": "essa é a opção 1",
+      "opcao_2": "essa é a opção 2",
+      "opcao_3": "essa é a opção 3",
+      "opcao_4": "essa é a opção 4"
+    }
+  },
+  "partida": {
+    "player_1": {
+      "jogador": {
+        "seu_turno": true,
+        "nome": "Daniel",
+        "posicao": 3,
+        "pino": "verde",
+        "questoes_respondidas": 3
       },
-      "2": {
-        "valor": 2,
-        "disponivel": false
-      },
-      "3": {
-        "valor": 3,
-        "disponivel": true
-      },
-      "4": {
-        "valor": 4,
-        "disponivel": true
-      },
-      "5": {
-        "valor": 5,
-        "disponivel": true
-      },
-      "6": {
-        "valor": 6,
-        "disponivel": true
+      "fichas" : {
+        "1": {
+          "valor": 1,
+          "disponivel": true
+        },
+        "2": {
+          "valor": 2,
+          "disponivel": false
+        },
+        "3": {
+          "valor": 3,
+          "disponivel": true
+        },
+        "4": {
+          "valor": 4,
+          "disponivel": true
+        },
+        "5": {
+          "valor": 5,
+          "disponivel": true
+        },
+        "6": {
+          "valor": 6,
+          "disponivel": true
+        }
       }
-    }
-  },
-  "player_2": {
-    "jogador": {
-      "nome": "Makoto",
-      "posicao": 13,
-      "pino": "amarelo"
-    }
-  },
-  "player_3": {
-    "jogador": {
-      "nome": "Felipe",
-      "posicao": 13,
-      "pino": "vermelho"
-    }
-  },
-  "player_4": {
-    "jogador": {
-      "nome": "Vanessa",
-      "posicao": 0,
-      "pino": "azul",
+    },
+    "player_2": {
+      "jogador": {
+        "nome": "Makoto",
+        "posicao": 13,
+        "pino": "amarelo",
+        "questoes_respondidas": 3
+      }
+    },
+    "player_3": {
+      "jogador": {
+        "nome": "Felipe",
+        "posicao": 13,
+        "pino": "vermelho",
+        "questoes_respondidas": 3
+      }
+    },
+    "player_4": {
+      "jogador": {
+        "nome": "Vanessa",
+        "posicao": 0,
+        "pino": "azul",
+        "questoes_respondidas": 3
+      }
     }
   }
 }
