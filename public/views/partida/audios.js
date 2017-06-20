b0abb1f0b1eb06ab600ceaf65f12d093cdb8cf37
Felipe Mariano <felipemarianodasilva@gmail.com>
2017-06-20 08:52:11 -0700

    adição imagens e audios

--- public/views/partida/audios.js
+++ public/views/partida/audios.js
@@ -4,3 +4,5 @@ var $pergunta_certa = new Audio('./../../assets/audios/i_have_the_power.mpeg');
 var $load_pergunta = new Audio('./../../assets/audios/lembra_dessa_porra_sim.mpeg');
 var $seu_turno = new Audio('./../../assets/audios/tava_mimindo.mpeg');
 var $turno_outros = new Audio('./../../assets/audios/nao_dorme.mpeg');
+var $fucking_shit = new Audio('./../../assets/audios/fucking_shit.m3');
+var $eh_nois_mano = new Audio('./../../assets/audios/eh_nois_mano.mp3');
